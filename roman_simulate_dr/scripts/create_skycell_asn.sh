b1e1c02cd92501a7ebfea163f14f0593a6024ed3
Mairan Teodoro <mteodoro@stsci.edu>
2025-11-13 11:01:04 -0500

    Implement skycell asn creator.

---
+++ roman_simulate_dr/scripts/create_skycell_asn.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Set output directory
+output_dir="RCAL-1168"
+
+# Set product types
+# (using array to avoid issues with word splitting)
+types=(visit pass full)
+
+for base in NO_TYPE "${types[@]}"; do
+
+  for dr in "" "_DR"; do
+
+    # Set target directory name
+    t="${base}${dr}"
+    # Create output directory
+    dir=$(echo "${output_dir}/${t}" | tr '[:lower:]' '[:upper:]')
+    mkdir -p "$dir"
+
+    # Set arguments based on type and data release
+    dr_arg=""
+    [[ $dr == "_DR" ]] && dr_arg="--data-release-id r0"
+    pt_arg=""
+    [[ $base != "NO_TYPE" ]] && pt_arg="--product-type $base"
+
+    # Loop over filters
+    for x in "$@"; do
+      echo "Processing all files for filter $x"
+      skycell_asn r*_"${x}"_cal.asdf -o r00001 $pt_arg $dr_arg && mv *.json "$dir/"
+    done
+
+  done
+
+done
